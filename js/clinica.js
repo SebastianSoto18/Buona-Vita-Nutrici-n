7c46d03aa35e7835ecc37a2427e1e553a6f50c4b
SebastianSoto18 <atlasjuanse12@gmail.com>
2022-06-27 05:39:44 -0500

    implementando funcionalidad del calculo del imc

---
+++ js/clinica.js
@@ -0,0 +1,16 @@
+var pacientes = document.querySelectorAll(".paciente");
+
+for(var i=0;i<pacientes.length;i++){
+     var paciente=pacientes[i];
+
+     var peso=paciente.querySelector(".info-peso").textContent;
+     var altura=paciente.querySelector(".info-altura").textContent;
+
+
+
+    if((peso > 0 && peso < 500) && (altura > 0 && altura < 2.80)){
+        paciente.querySelector(".info-imc").textContent=(peso/(altura*altura)).toFixed(2);
+    }else{
+        paciente.querySelector(".info-imc").textContent="Verifique peso y altura del paciente";
+    }
+}
