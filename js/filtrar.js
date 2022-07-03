7f1dcd236fec6e69514f0282578e4989fc7f19be
SebastianSoto18 <atlasjuanse12@gmail.com>
2022-07-03 11:53:40 -0500

    filtrado v1

--- js/filtrar.js
+++ js/filtrar.js
@@ -2,6 +2,20 @@ var campofiltro = document.querySelector('#filtrar');
 
 campofiltro.addEventListener('input', function(e) {
 
+    var pacientes = document.querySelectorAll('.paciente');
+
+    for (var i = 0; i < pacientes.length; i++) {
+       
+        var paciente = pacientes[i];
+        var nombre= paciente.querySelector('.info-nombre').textContent;
+        
+        if (nombre != this.value) {
+            paciente.classList.add('invisible');
+        }else{
+            paciente.classList.remove('invisible');
+        }
+    }
+
     
 
 });
