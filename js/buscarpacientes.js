e03c55165eafe9c9d72227e4515d2f9061eb5d73
SebastianSoto18 <atlasjuanse12@gmail.com>
2022-07-03 13:57:07 -0500

    manejo de errores en la peticion

--- js/buscarpacientes.js
+++ js/buscarpacientes.js
@@ -7,14 +7,22 @@ buton.addEventListener('click', function(e) {
     peticion.open("GET", "https://alura-es-cursos.github.io/api-pacientes/pacientes.json");
     
     peticion.addEventListener('load', function (){
+       
+       if(peticion.status == 200) {
+       
+       
         var respuesta= peticion.responseText;
 
         var pacientes = JSON.parse(respuesta);
 
         pacientes.forEach(function(paciente){
             addpaciente(paciente);
+        
         });
-
+        }else{
+            document.querySelector('#error-ajax').classList.remove('invisible');
+            console.log(peticion.status);
+        }
     });
 
     peticion.send();
