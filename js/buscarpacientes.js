bb2d06534a2f91d990d329a4e9421bcfc22e08d4
SebastianSoto18 <atlasjuanse12@gmail.com>
2022-07-03 13:43:51 -0500

    implementando llamada de datos por API v1.0

--- js/buscarpacientes.js
+++ js/buscarpacientes.js
@@ -7,7 +7,14 @@ buton.addEventListener('click', function(e) {
     peticion.open("GET", "https://alura-es-cursos.github.io/api-pacientes/pacientes.json");
     
     peticion.addEventListener('load', function (){
-        console.log(peticion.response);
+        var respuesta= peticion.responseText;
+
+        var pacientes = JSON.parse(respuesta);
+
+        pacientes.forEach(function(paciente){
+            addpaciente(paciente);
+        });
+
     });
 
     peticion.send();
