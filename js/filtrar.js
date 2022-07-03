154c7f232111fdd71342b5b145beb4aa34c4e82b
SebastianSoto18 <atlasjuanse12@gmail.com>
2022-07-03 13:09:50 -0500

    terminando funcionalidad v1.0.1

--- js/filtrar.js
+++ js/filtrar.js
@@ -4,18 +4,27 @@ campofiltro.addEventListener('input', function(e) {
 
     var pacientes = document.querySelectorAll('.paciente');
 
-    for (var i = 0; i < pacientes.length; i++) {
+    if(this.value.length > 0) {
+
+        for (var i = 0; i < pacientes.length; i++) {
        
-        var paciente = pacientes[i];
-        var nombre= paciente.querySelector('.info-nombre').textContent;
-        
-        if (nombre != this.value) {
-            paciente.classList.add('invisible');
-        }else{
-            paciente.classList.remove('invisible');
+            var paciente = pacientes[i];
+            var nombre= paciente.querySelector('.info-nombre').textContent;
+            
+            var expression = new RegExp(this.value,"i");
+
+            if (!expression.test(nombre)) {
+                    paciente.classList.add('invisible');
+            }else{
+                paciente.classList.remove('invisible');
+            }
+        }
+
+    }else{
+
+        for (var i = 0; i < pacientes.length; i++) {
+            pacientes[i].classList.remove('invisible');
         }
     }
-
-    
-
+ 
 });
